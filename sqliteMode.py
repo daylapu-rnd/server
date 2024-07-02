b72fe4e080f4016f711799779a89f44124bab164
Ayvers0 <maks.saf.val10@mail.ru>
2024-07-02 11:07:41 +0300

    DAY-4

--- sqliteMode.py
+++ sqliteMode.py
@@ -45,3 +45,11 @@ def InsertData(T, V, C=""):
         log_error(e)
         return []
 
+def SelectData(T, C, V, S="*"):
+    """Sending data from the database"""
+    try:
+        cur.execute(f'SELECT {S} FROM {T} WHERE {C} = "{V}"')
+        return [dict(zip([key[0] for key in cur.description], row)) for row in cur.fetchall()][0]
+    except Exception as e:
+        return []
+
